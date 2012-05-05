adbb0488867d392ab6ce59d3b84803167ff0d88f
Anna Warzecha <anna.warzecha@gmail.com>
2012-05-06 00:47:20 +0200

    Move to API v1.3, whitespace cleanup, setup cleanup

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
+
+version = __import__('pychimp').__version__
 
 setup(
     name='pychimp',
-    version=__import__('pychimp').__version__,
+    version=version,
     description='A reusable Django app for queuing the sending of email',
     author='Hunter Ford',
     author_email='hunterford@gmail.com',
